d5b079bf202d898abab0a7820577f2674c01e2ba
willliu1981 <h111107231675>
2026-02-04 02:40:35 +0800

    test hello world

--- core/src/main/java/idv/kuan/studio/libgdx/printstar/FirstScreen.java
+++ core/src/main/java/idv/kuan/studio/libgdx/printstar/FirstScreen.java
@@ -1,17 +1,41 @@
 package idv.kuan.studio.libgdx.printstar;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** First screen of the application. Displayed after the application is created. */
+
+/**
+ * First screen of the application. Displayed after the application is created.
+ */
 public class FirstScreen implements Screen {
+
+    private SpriteBatch spriteBatch;
+    private BitmapFont bitmapFont;
+
     @Override
     public void show() {
         // Prepare your screen here.
+        spriteBatch = new SpriteBatch();
+        bitmapFont = new BitmapFont();
+
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 0.1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spriteBatch.begin();
+        bitmapFont.draw(spriteBatch, "Hello World", 20.0f, 740.0f);
+        spriteBatch.end();
     }
 
     @Override
@@ -37,5 +61,11 @@ public class FirstScreen implements Screen {
     @Override
     public void dispose() {
         // Destroy screen's assets here.
+        if (bitmapFont != null) {
+            bitmapFont.dispose();
+        }
+        if (spriteBatch != null) {
+            spriteBatch.dispose();
+        }
     }
 }
